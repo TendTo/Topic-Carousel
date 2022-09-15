a8271267ff7f5622da180852cbeb5124936189e4
Ernesto Casablanca <casablancaernesto@gmail.com>
2022-09-15 17:48:24 +0200

    test(tests): rename test

--- tests/e2e/loadPage.test.ts
+++ tests/e2e/loadPage.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('the button changes the content afte being clicked', async function ({ page }) {
+test('the pages loads correctly', async function ({ page }) {
   await page.goto('/');
 
   await page.evaluate(() => new TopicCarousel.TopicCarousel());
